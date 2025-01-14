297a3a46204a669b6deccbc440277d89cb18bfb5
bhoomikabadala <bhoomikabadala1806@gmail.com>
2025-01-14 18:03:46 +0530

    printf

---
+++ src/ex_java_programs/Lab002_printf.java
@@ -0,0 +1,26 @@
+package ex_java_programs;
+
+public class Lab002_printf {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("%d X 1 = 9",num);
+        System.out.println();
+        System.out.printf("%d X 2 = 18",num);
+        System.out.println();
+        System.out.printf("%d X 3 = 27",num);
+        System.out.println();
+        System.out.printf("%d X 4 = 36",num);
+        System.out.println();
+        System.out.printf("%d X 5 = 45",num);
+        System.out.println();
+        System.out.printf("%d X 6 = 54",num);
+        System.out.println();
+        System.out.printf("%d X 7 = 63",num);
+        System.out.println();
+        System.out.printf("%d X 8 = 72",num);
+        System.out.println();
+        System.out.printf("%d X 9 = 81",num);
+        System.out.println();
+        System.out.printf("%d X 10 = 90",num);
+    }
+}
