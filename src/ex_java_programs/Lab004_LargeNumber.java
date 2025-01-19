c72823620e576f9349c9eeb995185a081d692535
bhoomikabadala <bhoomikabadala1806@gmail.com>
2025-01-19 20:38:15 +0530

    19th Jan changes

---
+++ src/ex_java_programs/Lab004_LargeNumber.java
@@ -0,0 +1,10 @@
+package ex_java_programs;
+
+public class Lab004_LargeNumber {
+    public static void main(String[] args) {
+        int x=100;
+        int y=20;
+        int largeint = x>y ? x : y;
+        System.out.println(largeint);
+    }
+}
---
+++ src/ex_java_programs/Lab005_EvenOdd.java
@@ -0,0 +1,9 @@
+package ex_java_programs;
+
+public class Lab005_EvenOdd {
+    public static void main(String[] args) {
+        int x=0;
+        String res = x%2==0 ? "Even" : "Odd";
+        System.out.println(res);
+    }
+}
