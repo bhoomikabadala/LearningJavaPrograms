982324f8d6bf484f0c8946b087708913934551dc
bhoomikabadala <bhoomikabadala1806@gmail.com>
2025-02-14 20:40:46 +0530

    14th Feb changes

--- src/ex_java_programs/Lab048_WhileEvenNos1to20.java
+++ src/ex_java_programs/Lab048_WhileEvenNos1to20.java
@@ -1,6 +1,6 @@
 package ex_java_programs;
 
-public class Lab48_WhileEvenNos1to20 {
+public class Lab048_WhileEvenNos1to20 {
     public static void main(String[] args) {
         int i=1;
         while(i<=20){
